a7c03aab120516b72e0807e8b507272507c07fa9
PlanetSat0rn <109532054+PlanetSat0rn@users.noreply.github.com>
2024-04-24 06:18:37 -0400

    actually remembered to fix the local repository

---
+++ Source/BrokenRim/BrokenRim/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BrokenRim
+{
+    [StaticConstructorOnStartup]
+    public static class TestClass
+    {
+
+         static TestClass()
+        {
+            Log.Message("hello chat");
+        }
+    }
+}
