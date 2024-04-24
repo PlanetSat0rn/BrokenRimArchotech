1499b0dae33646d4c083a841145f8341afedf2f0
PlanetSat0rn <109532054+PlanetSat0rn@users.noreply.github.com>
2024-04-24 08:22:58 -0400

    don't you hate it when your own people leaves you behind?

---
+++ Source/BrokenRim/BrokenRim/Archopawn_ThoughtWorker.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Diagnostics;
+using Verse;
+
+namespace BrokenRimArchotech
+{
+    public class Archopawn_ThoughtWorker : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            if (p.def.defName != "Archopawn")
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
